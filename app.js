8dbee96e0e911e1c66ef3a5d7faea088126a8659
Nishthakumari <knishtha0@gmail.com>
2022-08-21 20:39:06 +0530

    Parsing JSON

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const { STATUS_CODES } = require("http");
 const app= express();
 const https= require("https");
 
@@ -8,7 +9,16 @@ app.get("/", function(req, res)
     const url= "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8be0f3ca418e441cd731f5d433ac0bb7";
     https.get(url, function(response)
     {
-        console.log(response);
+        console.log(response.statusCode);
+        response.on("data", function(data)
+        {
+            const weatherData= JSON.parse(data);
+            const temp= weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            console.log(description);
+        })
+
+
     })
     res.send("Server is up and running");
 })
