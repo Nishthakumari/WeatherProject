8a88ee6ef0b70905b422afca50c39f740de858c4
Nishthakumari <knishtha0@gmail.com>
2022-08-21 22:01:20 +0530

    Using Express to Render the Website with Live API Data

--- app.js
+++ app.js
@@ -15,12 +15,17 @@ app.get("/", function(req, res)
             const weatherData= JSON.parse(data);
             const temp= weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+            res.write("<p>The weather is currently "+ description + "</p>");
+            res.write("<h1>The temperature in london is " +  temp + " degree Celcuis </h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
         })
 
 
     })
-    res.send("Server is up and running");
+    
 })
 
 app.listen(3000, function()
